13eb5a2c3bf82be0d612162c45ad1475912e9e85
kjxlstad <jonathan.kjolstad@gmail.com>
2023-10-12 23:04:52 +0200

    corrected typo

--- jaxisp/nodes/scl.py
+++ jaxisp/nodes/scl.py
@@ -6,6 +6,6 @@ from jax.image import resize
 from jaxisp.nodes.common import ISPNode
 
 
-class AWB(ISPNode):
+class SCL(ISPNode):
     def compile(self, width: int, height: int, **kwargs):
         return jit(partial(resize, shape=(height, width, 3), method="linear"))
